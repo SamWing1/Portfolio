763d9a6c325b369955a2ee6293aa1ab999ae0688
SamWing1 <geckoromanwing@hotmail.com>
2023-01-13 13:37:13 -0800

    styled about page

--- src/pages/AboutMe/AboutMe.jsx
+++ src/pages/AboutMe/AboutMe.jsx
@@ -3,9 +3,11 @@ import './AboutMe.css';
 export default function AboutMe() {
     return (
         <>
-            <h1>The Journey So Far, and Where it's Going</h1>
-            <p>From dishwashing to fueling planes, I’ve worked across a broad field of varying industries, and gathered a comprehensive and unique set of viewpoints and experiences from them. Now with Software Engineering I’ve found a profession and skill set that I know I can bring all those previous experiences to bear on, as I settle into a lifetime of sharpening my coding and software development skills. My work and curiosity is driven by my passion for developing a craft. And this commitment can be shown through each challenge I solve, approaching it as a chance to dig into a new concept or ideal! I am self motivated and can deliver work independently, but I also enjoy lending my expertise to, and collaborating with a team of my peers! I bring a positive attitude and rigorous work ethic to both.</p>
-            <p>Technologies and Frameworks I've worked with include: HTML, CSS, Javascript, Python, Django, MongoDB, Node.js, Express.js, React.js, Mongoose, SQL</p>
+            <h1 id='aboutheader'>The Journey So Far, and Where it's Going</h1>
+            <div id='abouttext'>
+                <p>From dishwashing to fueling planes, I’ve worked across a broad field of varying industries, and gathered a comprehensive and unique set of viewpoints and experiences from them. Now with Software Engineering I’ve found a profession and skill set that I know I can bring all those previous experiences to bear on, as I settle into a lifetime of sharpening my coding and software development skills. My work and curiosity is driven by my passion for developing a craft. And this commitment can be shown through each challenge I solve, approaching it as a chance to dig into a new concept or ideal! I am self motivated and can deliver work independently, but I also enjoy lending my expertise to, and collaborating with a team of my peers. I bring a positive attitude and rigorous work ethic to both.</p>
+            </div>
+            <p id='abouttechs'>Technologies and Frameworks I've worked with include: HTML, CSS, Javascript, Python, Django, MongoDB, Node.js, Express.js, React.js, Mongoose, SQL</p>
         </>
     )
 }
