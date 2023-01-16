02332fa90eaa29270c4aa211cfc64e959ae9086c
SamWing1 <geckoromanwing@hotmail.com>
2023-01-16 13:27:27 -0800

    formatting contact page for mobile

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -10,32 +10,39 @@ export default function Contact() {
     return(
         <>
             <h1 id='reachme'>Where to Reach Me!</h1>
-            <div class='card'>
-                <img class='pic' src={headshot}/>
-                <div class='cardtext'>Sam Wing <br /> geckoromanwing@hotmail.com <br /> Santa Barbara, CA</div>
-                <a href='https://docs.google.com/document/d/10-dfrRsxo8AhhJfeyRZWXgxZnhnSDawJN1PRaWR0qJU/edit?usp=sharing' target='_blank' rel='noreferrer noopener'><img class='resume' src={resume}/></a>
-                <p class='restxt'>resume</p>
-            </div>
-            <div class='container'>
-                <div class='subcontainer'>
-                    <div class='phonestub'>
-                        <img class='phoneimg' src={cellphone}/>
-                        <p class='stubtext'>Call or Text at <br />805-886-1708</p>
+            <div class='bigcontainer'>
+                <div class='card'>
+                    <img class='pic' src={headshot}/>
+                    <div class='cardtext'>Sam Wing <br /> geckoromanwing@hotmail.com <br /> Santa Barbara, CA</div>
+                    <a href='https://docs.google.com/document/d/10-dfrRsxo8AhhJfeyRZWXgxZnhnSDawJN1PRaWR0qJU/edit?usp=sharing' target='_blank' rel='noreferrer noopener'><img class='resume' src={resume}/></a>
+                    <p class='restxt'>resume</p>
+                </div>
+                <div class='container'>
+                    <div class='subcontainer'>
+                        <div class='phonestub'>
+                            <img class='phoneimg' src={cellphone}/>
+                            <p class='stubtext'>Call or Text at <br />805-886-1708</p>
+                        </div>
+                        <div class='linkstub'>
+                            <a href='https://www.linkedin.com/in/sam-wing-5b132324a/' target='_blank' rel='noreferrer noopener'>
+                                <img class='linkedimg' src={linkedin} />
+                            </a>
+                            <p class='stubtext'>Check out my LinkedIn</p>
+                        </div>
+                        <div class='gitstub'>
+                            <a href='https://github.com/SamWing1' target='_blank' rel='noreferrer noopener'><img class='gitimg' src={github}/></a>
+                            <p class='stubtext'>Github</p>
+                            
+                        </div>
                     </div>
-                    <div class='linkstub'>
-                        <a href='https://www.linkedin.com/in/sam-wing-5b132324a/' target='_blank' rel='noreferrer noopener'>
-                            <img class='linkedimg' src={linkedin} />
-                        </a>
-                        <p class='stubtext'>Check out my LinkedIn</p>
-                    </div>
-                    <div class='gitstub'>
-                        <a href='https://github.com/SamWing1' target='_blank' rel='noreferrer noopener'><img class='gitimg' src={github}/></a>
-                        <p class='stubtext'>Github</p>
-                        
+                    <div class='emailform'>
+                        <ContactUs />
                     </div>
                 </div>
-                <ContactUs />
             </div>
+            <div class='mobileform'>
+                    <ContactUs />
+                </div>
         </>
     )
 }
